1209d94147333e3c2abece03a26ff0062df6b9ca
Wilson Ong <wilsonong.hs@gmail.com>
2024-01-30 15:51:35 +0800

    Added hashmap for char to value and changed getters

--- src/KeyTable.java
+++ src/KeyTable.java
@@ -1,13 +1,26 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class KeyTable {
     private char[] keyTable = new char[]{'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S',
             'T','U','V','W','X','Y','Z','0','1','2','3','4','5','6','7','8','9','(',')','*','+',',','-','.','/'};
 
+    private Map<Character,Integer> letterTable;
+
     protected KeyTable() {
+        letterTable = new HashMap<>();
+        for (int i = 0; i < keyTable.length; i++) {
+            letterTable.put(keyTable[i],i);
+        }
 
     }
 
-    protected char[] getTable() {
-        return keyTable;
+    protected char convertValueToChar(int value) {
+        return keyTable[value];
+    }
+
+    protected int convertCharToValue(Character c) {
+        return letterTable.getOrDefault(c,-1);
     }
 
 }
