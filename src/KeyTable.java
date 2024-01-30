05e92796a525a0fd305dfe50ab6ce85a54a9c1c7
Wilson Ong <wilsonong.hs@gmail.com>
2024-01-30 15:37:32 +0800

    Added getter and constructor for keytable

--- src/KeyTable.java
+++ src/KeyTable.java
@@ -1,8 +1,13 @@
 public class KeyTable {
-    private char[] keyTable;
+    private char[] keyTable = new char[]{'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S',
+            'T','U','V','W','X','Y','Z','0','1','2','3','4','5','6','7','8','9','(',')','*','+',',','-','.','/'};
 
-    public  KeyTable() {
-        keyTable = new char[]{'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S',
-                'T','U','V','X','Y','Z','0','1','2','3','4','5','6','7','8','9','(',')','*','+',',','-','.','/'};
+    protected KeyTable() {
+
     }
+
+    protected char[] getTable() {
+        return keyTable;
+    }
+
 }
