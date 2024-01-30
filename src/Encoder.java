9bb0c7333a611233c715360e6332f6e5ba08c78f
Wilson Ong <wilsonong.hs@gmail.com>
2024-01-30 16:06:41 +0800

    Added encoder class

--- src/Encoder.java
+++ src/Encoder.java
@@ -1,2 +1,26 @@
+import java.util.Random;
+
 public class Encoder {
+
+    private KeyTable keyTable;
+
+    public Encoder() {
+        keyTable = new KeyTable();
+    }
+
+    public String encode(String plainText) {
+        StringBuilder encodedText = new StringBuilder();
+
+        int offsetNum = (int)Math.random()*44;
+        char offsetChar = keyTable.convertValueToChar(offsetNum);
+
+        for(int i=0; i<plainText.length();i++) {
+            char c = plainText.charAt(i);
+
+        }
+    }
+
+    public String decode(String plainText) {
+
+    }
 }
