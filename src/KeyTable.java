d060e945a718dae93fffa6000d4cb35c148c352d
Wilson Ong <wilsonong.hs@gmail.com>
2024-01-30 15:30:09 +0800

    First commit - created KeyTable class

---
+++ src/KeyTable.java
@@ -0,0 +1,8 @@
+public class KeyTable {
+    private char[] keyTable;
+
+    public  KeyTable() {
+        keyTable = new char[]{'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S',
+                'T','U','V','X','Y','Z','0','1','2','3','4','5','6','7','8','9','(',')','*','+',',','-','.','/'};
+    }
+}
